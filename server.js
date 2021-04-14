987ee6b2d7265d97cd0a26a3a33bc86130e1df95
Willy <willy.rineau2018@campus-eni.fr>
2021-04-14 22:48:09 +0200

    Add for Heroku integration

---
+++ server.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+const buildPath = path.join(__dirname, '..', 'build');
+app.use(express.static(buildPath));
+
+app.listen(PORT, () => {
+    console.log(`server started on port ${PORT}`);
+});
