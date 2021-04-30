b5eab7c0424cf76054f1d673b405158d796547bc
JRME-NA <jeremy.enet2017@campus-eni.fr>
2021-04-30 11:22:03 +0200

    Page import user
    A terminer

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,7 @@
 module.exports = {
   someSidebar: {
     'Docusaurus': ['getting-started', 'create-a-page'],
+    'Application Datafleet': ['import-users'],
     'Outils externes': ['logrocket'],
   },
 };
